9f73cc5d182796f7d1427e77546962172195b40c
naleilan <7776224+naLeilan@users.noreply.github.com>
2024-07-01 11:39:17 +0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,28 @@
+import { useState } from "react";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+
 function App() {
-  return <div>App</div>;
+  const [mapPosition, setMapPosition] = useState([40, 0]);
+  return (
+    <div className="mapContainer">
+      <MapContainer
+        center={mapPosition}
+        zoom={9}
+        scrollWheelZoom={true}
+        className="map"
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+        />
+        <Marker position={mapPosition}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </div>
+  );
 }
 
 export default App;
